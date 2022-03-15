3af6b0f476dd3e118b8cca75829d5f5110bdb989
adscud <arthurdelbeke@smartness.co>
2022-03-15 23:27:54 +0100

    feat: get device token

--- App.tsx
+++ App.tsx
@@ -1,7 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import React from "react";
+import { default as Constants } from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  React.useEffect(() => {
+    async function registerForPushNotificationsAsync() {
+      let token;
+      if (Constants.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+          alert('Failed to get push token for push notification!');
+          return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+      } else {
+        alert('Must use physical device for Push Notifications');
+      }
+
+      console.info('Device token =>', token);
+    }
+    void registerForPushNotificationsAsync()
+
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
